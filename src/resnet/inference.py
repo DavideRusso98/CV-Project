25d8e9bf4331c28fbeed2d37722e1d1347508aaa
riccardo_prini00 <274292@studenti.unimore.it>
2024-07-04 11:57:15 +0200

    Inference fixed ( it's detecting two bboxes A CASO)

--- src/resnet/inference.py
+++ src/resnet/inference.py
@@ -1,13 +1,14 @@
 import torch
 import torchvision.transforms as T
 from PIL import Image
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 from torchvision.models.detection import keypointrcnn_resnet50_fpn
 
+root = "/home/riccardo/Scrivania/UNIVERSITA/Computer Vision and Cognitive Systems/CV-Project/src/resnet"
 # Loading pre-trained model
 model = keypointrcnn_resnet50_fpn(pretrained=False, num_classes=2, num_keypoints=20)
-model.load_state_dict(torch.load('/mnt/c/Users/alesv/PycharmProjects/CV-Project/our_super_model_trained.pth'))
+model.load_state_dict(torch.load(f'{root}/automotive_keypoint_detector.pth', map_location=torch.device('cpu')))
 model.eval()
 
 # Preprocessing image function
@@ -20,30 +21,53 @@ def preprocess_image(image_path):
     return image
 
 # Percorso dell'immagine
-image_path = "/mnt/c/Users/alesv/PycharmProjects/CV-Project/src/dataset/output/images/clean/alfa_0.jpg"
+image_path = "./src/dataset/output/images/clean/alfa_56.jpg"
 image = preprocess_image(image_path)
 
 # Effettua l'inferenza
 with torch.no_grad():
     prediction = model([image])
-def plot_keypoints(image, keypoints, scores, threshold=0.5):
+
+def plot_keypoints(image, keypoints, scores, threshold=13):  # Soglia impostata a 0.5
     plt.imshow(image.permute(1, 2, 0).cpu().numpy())
     ax = plt.gca()
     for i, keypoint_set in enumerate(keypoints):
-        if scores[i] > threshold:
-            for keypoint in keypoint_set:
-                x, y, v = keypoint
-                print(keypoint)
-                if v > 0:  # keypoint visibile
-                    circle = plt.Circle((x, y), 2, color='red', fill=True)
-                    ax.add_patch(circle)
+        for j, keypoint in enumerate(keypoint_set):
+            x, y, v = keypoint
+            if v > 0 and scores[0][j] > threshold:  # 0 perchè guardiamo solo alla bbox della immagine
+                circle = plt.Circle((x, y), 2, color='red', fill=True)
+                ax.add_patch(circle)
+    plt.axis('off')
+    plt.show()
+
+def plot_predictions(image, predictions):
+    plt.imshow(image.permute(1, 2, 0).cpu().numpy())
+    ax = plt.gca()
+    boxes = predictions[0]['boxes'].cpu()
+    for box in boxes:
+        x1, y1, x2, y2 = box
+        rect = plt.Rectangle((x1, y1), x2 - x1, y2 - y1, fill=False, color='blue')
+        ax.add_patch(rect)
     plt.axis('off')
     plt.show()
 
 # Visualizza i risultati
 image = image.cpu()
+# plot_predictions(image, prediction)
 keypoints = prediction[0]['keypoints'].cpu()
-scores = prediction[0]['scores'].cpu()
-print(keypoints)
-print(scores)
-plot_keypoints(image, keypoints, scores)
+scores = prediction[0]['keypoints_scores'].cpu()
+# print(prediction)
+
+# Calcola statistiche descrittive
+all_scores = scores[0].flatten().numpy()
+mean_score = all_scores.mean()
+median_score = np.median(all_scores)
+min_score = all_scores.min()
+max_score = all_scores.max()
+
+print(f"Mean Score: {mean_score}")
+print(f"Median Score: {median_score}")
+print(f"Min Score: {min_score}")
+print(f"Max Score: {max_score}")
+
+plot_keypoints(image, keypoints, scores, mean_score)
