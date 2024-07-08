2409bd188d670526b5adae9e60855984a26b2728
DavideRusso98 <daviderusso125@gmail.com>
2024-07-09 00:57:43 +0200

    img-processing stable pipeline

--- src/img-processing/process_img.py
+++ src/img-processing/process_img.py
@@ -9,25 +9,25 @@ def preprocess_image(image_path, output_path):
     image = cv2.imread(image_path)
     image = denoise_image(image)
     image = enhance_contrast(image)
-    # image = sharpen_image(image)
-    # image = normalize_image(image)
+    image = sharpen_image(image)
+    image = normalize_image(image)
+    #image = remove_reflections(image)
     
-    # Salva l'immagine elaborata
     cv2.imwrite(output_path, image)
 
 def denoise_image(image):
     denoised = cv2.bilateralFilter(image, d=9, sigmaColor=75, sigmaSpace=75)
-    # denoised = cv2.GaussianBlur(image, (5, 5), 0)
+    #denoised = cv2.GaussianBlur(image, (5, 5), 0)
     return denoised
 
 def enhance_contrast(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    enhanced_gray = cv2.equalizeHist(gray)
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    enhanced_gray = clahe.apply(gray)
     yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
     yuv[:, :, 0] = enhanced_gray
     enhanced_image = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
-    resized_image = cv2.resize(enhanced_image, (800, 800))
-    return resized_image
+    return enhanced_image
 
 def sharpen_image(image):
     kernel = np.array([[0, -1, 0],
@@ -39,6 +39,15 @@ def sharpen_image(image):
 def normalize_image(image):
     normalized = cv2.normalize(image, None, 0, 255, cv2.NORM_MINMAX)
     return normalized
+
+def remove_reflections(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    _, mask = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)  # Apply a binary threshold to create a mask for reflections
+    
+    inpainted = cv2.inpaint(image, mask, 3, cv2.INPAINT_TELEA)  # Inpainting to remove reflections
+    
+    return inpainted
 
 def process_images(input_folder, output_folder):
     if not os.path.exists(output_folder):
